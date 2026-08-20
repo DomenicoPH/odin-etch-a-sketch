dd1c26062d72c312bcb18c28fe9f4bb25172a873
Domenico Pagano <dpaganoh@gmail.com>
2026-08-20 15:11:06 -0500

    DONE - etch-a-sketch project done

--- script.js
+++ script.js
@@ -2,10 +2,32 @@ let gridSize = 16;
 const grid = document.querySelector('#grid');
 const gridSizeInput = document.querySelector('#grid-size');
 const createBtn = document.querySelector('#create-grid-btn');
+const resetBtn = document.querySelector('#reset-grid-btn');
 const alert = document.querySelector('.alert');
 const errorText = document.querySelector('#error-text');
 const closeBtn = document.querySelector('#close-btn');
 
+let isDrawing = false;
+
+// ---Paint the grid boxes when the mouse is pressed and held while moving over them
+grid.addEventListener('mousedown', (e) => {
+    if (e.target.classList.contains('grid-box')) {
+        isDrawing = true;
+        e.target.style.backgroundColor = 'black';
+    }
+});
+
+grid.addEventListener('mouseover', (e) => {
+    if (isDrawing && e.target.classList.contains('grid-box')) {
+        e.target.style.backgroundColor = 'black';
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    isDrawing = false;
+});
+
+// ---Create grid
 gridSizeInput.addEventListener('change', (e) => {
     gridSize = e.target.value;
 });
@@ -15,10 +37,18 @@ createBtn.addEventListener('click', () => {
     createGrid(gridSize);
 });
 
+// ---Reset grid
+resetBtn.addEventListener('click', () => {
+    resetGrid();
+});
+
+// ---Close alert box
 closeBtn.addEventListener('click', () => {
     alert.style.display = 'none';
 });
 
+
+// Functions
 function createGrid(gridSize){
     if(gridSize > 100){
         alert.style.display = 'flex';
@@ -41,6 +71,13 @@ function createGrid(gridSize){
             grid.appendChild(row);
         }
     }
+}
+
+function resetGrid(){
+    const gridBoxes = document.querySelectorAll('.grid-box');
+     gridBoxes.forEach((gridBox) => {
+        gridBox.style.backgroundColor = '';
+     });
 }
 
 function clearGrid(){
